45f9b46326d3ea5d33ad9ebabed97e8872ecd02c
jesusbolivar94 <55284386+jesusbolivar94@users.noreply.github.com>
2022-08-25 00:54:32 -0400

    Update authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const authApi = axios.create({
     baseURL: 'https://identitytoolkit.googleapis.com/v1/accounts',
     params: {
-        key: 'AIzaSyBFhtclvCt1wr4R1I-GOJcy5DDV1xh2vLg'
+        key: '?'
     }
 })
 
